042e196aad9f7c4fe59942e1c2774caf993690ad
Shraddha141 <shraddhasalve1709@gmail.com>
2021-05-31 10:56:46 +0530

    new code added

---
+++ Shraddha/jscript/student.js
@@ -0,0 +1,164 @@
+
+      const students=[
+	  {
+      	roll_no:1,
+      	name:"A",
+      	age:22,
+		address:
+		{
+		plot_no:11,
+		area:"Jail Road",
+		landmark:"Abhinav School",
+		city:"Nashik",
+		state:"Maharashtra",
+		country:"India"
+		},
+      	},
+
+       {
+      	roll_no:2,
+      	name:"B",
+      	age:18,
+	      	address:
+	      	{
+	      		plot_no:3,
+				
+	      		area:"Apoorva Colony",
+	      		landmark:"Mahadev Mandir",
+	      		city:"Nashik",
+                state:"Maharashtra",
+				country:"India"
+	      	},
+      	},
+ 
+       {
+      	roll_no:3,
+      	name:"C",
+      	age:20,
+	      	address:
+	      	{
+	      		plot_no:5,
+				
+	      		area:"Datta Mandir",
+	      		landmark:"City Hospital",
+				city:"Thane",
+                state:"Maharashtra",
+				country:"India"
+	      	},
+      	},
+
+
+        {
+      	roll_no:4,
+      	name:"D",
+      	age:24,
+	      	address:
+	      	{
+	      		plot_no:20,
+				
+	      		area:"Ganesh Colony",
+	      		landmark:"Civil Hospital",
+	      		city:"Jalgaon",
+                state:"Maharashtra",
+				country:"India"
+	      	},
+      	},
+
+       {
+      	roll_no:5,
+      	name:"E",
+      	age:19,
+	      	address:
+	      	{
+	      		plot_no:5,
+				
+	      		area:"Pacchim Vihar",
+	      		landmark:"City Hospital",
+	      		city:"New Delhi",
+                state:"New Delhi",
+				country:"India"
+	      	},
+      	
+        },
+
+        {
+      	roll_no:6,
+      	name:"F",
+      	age:23,
+	      	address:
+	      	{
+	      		plot_no:10,
+				
+	      		area:"Laxmi Nagar",
+	      		landmark:"Ganesh Mandir",
+	      	    city:"Lucknow",
+                state:"Uttar Pradesh",
+				country:"India"
+	      	},
+      	},
+
+      {
+      	roll_no:7,
+      	name:"G",
+      	age:24,
+	      	address:
+	      	{
+	      		plot_no:12,
+				
+	      		area:"Sarojini Nagar",
+	      		landmark:"Metro Mall",
+				city:"Chandigarh",
+				state:"Punjab",
+				country:"India"
+	      	},
+      	},
+
+       {
+      	roll_no:8,
+      	name:"H",
+      	age:15,
+	      	address:
+	      	{
+	      		plot_no:14,
+			
+	      		area:"Mall Road",
+	      		landmark:"Biker's Point",
+	      		city:"Mussoorie",
+                state:"Uttarakhand",
+				country:"India"
+	      	},
+      	},
+
+      	{
+
+      	roll_no:9,
+      	name:"I",
+      	age:20,
+	      	address:
+	      	{
+	      		plot_no:20,
+				
+	      		area:"Pahad Ganj",
+	      		landmark:"Citi Bank",
+	      		city:"Agra",
+                state:"Uttar Pradesh",
+				country:"India"
+	      	},
+      	},
+
+       {
+      	roll_no:10,
+      	name:"J",
+      	age:27,
+	      	address:
+	      	{
+	      		plot_no:30,
+	      		area:"MG Road",
+	      		landmark:"MG Memorial Hospital",
+	      		city:"Kolkata",
+                state:"West Begal",
+				country:"India"
+	      	      	}
+      	
+      }]
+	console.log(students)
---
+++ Shraddha/jscript/student_mgt.js
@@ -0,0 +1,76 @@
+function onPageLoad() {
+	displayStudents(students);
+}
+
+function onAddStudent() {
+	const add_student_modal_element = document.getElementById("add_student_modal");
+	const form_element = add_student_modal_element.querySelector("form");
+	
+	const roll_no = parseInt(form_element.roll_number.value);
+	const name = form_element.name.value;
+	const age = parseInt(form_element.age.value);
+	const plot_number = parseInt(form_element.plot_number.value);
+	const area = form_element.area.value;
+	const landmark = form_element.landmark.value;
+	const city = form_element.city.value;
+	const state = form_element.state.value;
+	const country = form_element.country.value;
+	
+	const new_student = {
+		roll_no: roll_no,
+		name: name,
+		age: age,
+		address:{
+			plot_no: plot_number,
+			area: area,
+			landmark: landmark,
+			
+			state: state,
+			country: country
+		}
+	}
+	add(new_student);
+	$('#add_student_modal').modal('hide')
+}
+
+function add(new_student) {
+	students.push(new_student);
+	displayStudents(students);
+}
+
+function update(student_id, params) {
+	
+}
+
+function deleteStudent(student_id) {
+	
+}
+
+function displayStudents(student_array) {
+	
+	const student_table_element = document.getElementById("student_table");
+	const student_table_body_element = student_table_element.querySelector("tbody")
+	for (let i=0; i<student_array.length; i++) {
+		let student_row = document.createElement("tr");
+		student_row.id = "student_" + student_array[i].roll_no;
+		
+		student_row.innerHTML = `
+			<td>${student_array[i].roll_no}</td>
+			<td>${student_array[i].name}</td>
+			<td>${student_array[i].age}</td>
+			<td>${student_array[i].address.plot_no},
+				${student_array[i].address.area},
+				${student_array[i].address.landmark},
+				${student_array[i].address.city},
+				${student_array[i].address.state},
+				${student_array[i].address.country}.
+			</td>`;
+		student_table_body_element.appendChild(student_row);
+	}
+	
+}
+
+
+function filterStudents(column, value) {
+	
+}
