4325fdc08dc4047d1dcd172fcdf2a68117fd02a0
Pallavi Mahajan <pallavimaha5@gmail.com>
2021-06-07 23:06:16 +0530

    to_do_list

--- Pallavi/javascript/to_do_list/item_list.js
+++ Pallavi/javascript/to_do_list/item_list.js
@@ -2,7 +2,7 @@ const to_do_list=[
 	{
 		item_id: 1,
 		item_name: "item 1",
-		completed: false,
+		completed: true,
 		steps: ["Step 1", "Step 2"]
 	},
 
@@ -16,7 +16,7 @@ const to_do_list=[
 	{
 		item_id: 3,
 		item_name: "item 3",
-		completed: false,
+		completed: true,
 		steps: []
 	},
 
@@ -36,7 +36,8 @@ const to_do_list=[
 ];
 
 function onPageLoad(){
-  hideAddForm();    
+  hideAddForm();  
+  DisplayList(to_do_list)  
 }
 
 function onShowAddForm(){
@@ -59,7 +60,7 @@ function hideAddForm(){
 
 function DisplayList(list){
 	const to_do_list_element=document.getElementById("to_do_list");
-	to_do_list.innerHTML="";
+	to_do_list_element.innerHTML="";
 
 	for(let i=0; i<list.length; i++){
 		const item = list[i];
@@ -81,7 +82,69 @@ function DisplayList(list){
 		 }
 		 cb.onchange=onItemStatusChange;
 
-		 const item_name=item.createElement("span");
-		 item_name.innerHTML=item.name;
+		 const item_name=document.createElement("span");
+		 item_name.innerHTML=item.item_name;
+       
+		 const due_date=document.createElement("span");
+		 due_date.innerHTML=item.due_date;
+
+		 const steps_ul = document.createElement("ul");
+		item.steps.forEach(
+			function(step) {
+				const step_li = document.createElement("li");
+				step_li.innerHTML = step;
+				steps_ul.appendChild(step_li)
+			}
+		)
+		   list_item.appendChild(cb);
+		   list_item.appendChild(item_name);
+		   list_item.appendChild(due_date);
+		   list_item.appendChild(steps_ul);
+
+		   to_do_list_element.appendChild(list_item);
 	}
+}
+
+function onAddStep(){
+	const steps_div=document.getElementById("steps");
+	const step_input=document.createElement("input");
+	step_input.type="text";
+	step_input.classList=["form-control"];
+	step_input.placeholder="add step description";
+
+	const step_div=document.createElement("div")
+	steps_div.classList=["row task-step-div"];
+
+	const steps_div_1=document.createElement("div");
+	steps_div_1.classList=["col-1"]
+
+	const steps_div_2=document.createElement("div");
+	steps_div_2.classList=["col-11"]
+
+	const number_span=document.createElement("span");
+	const task_step_divs=steps_div.querySelectorAll(".task-step-div")
+     number_span.innerHTML=task_step_divs.length;
+     //console.log(number_span)
+
+     steps_div_1.appendChild(number_span);
+     steps_div_2.appendChild(step_input);
+
+     step_div.appendChild(steps_div_1);
+     step_div.appendChild(steps_div_2);
+
+     steps_div.appendChild(step_div);
+}
+
+function onItemStatusChange(event) {
+	console.log("onItemStatusChange ", event.target.value, event.target.checked);
+	
+	const index = to_do_list.findIndex(
+		function(todo_item) {
+			return todo_item.item_id == event.target.value;
+		}
+	);
+	
+	console.log("index ==> ", index);
+	to_do_list[index].completed = event.target.checked;
+	DisplayList(to_do_list);
 }
